ab7191d18b2e139b19b7aff9c72dbe5478bc1af5
aaryanbhattarai <126233049+aaryanbhattarai@users.noreply.github.com>
2024-11-28 16:47:41 +0545

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubbelsortprogram
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 11, 15, 14, 23, 22, 19 };
+            SortAlgo sortAlgo = new SortAlgo();
+
+            Console.WriteLine("Before Sorting:");
+            sortAlgo.displayArray(array);
+
+            sortAlgo.bubbleSort(array);
+
+            Console.WriteLine("After Sorting:");
+            sortAlgo.displayArray(array);
+        }
+    }
+}
+    
+
