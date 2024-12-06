8f8ec31e275bb9aef1f6f02139104056f1bb6b30
Aaryan Bhattarai <baaryan326@gmail.com>
2024-12-06 07:59:32 +0545

    Desktop Calculator Assignment

---
+++ Calculator/Answer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Answer : Form
+    {
+
+        public Answer(string result)
+        {
+            InitializeComponent();
+            this.label1.Text = result;
+        }
+
+
+        public Answer()
+        {
+            InitializeComponent();
+        }
+
+        private void Answer_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
