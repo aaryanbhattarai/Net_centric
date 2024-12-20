0ea52fb10166cf13d6f09ea8c7bfe4715cbad164
Aaryan Bhattarai <baaryan326@gmail.com>
2024-12-20 08:05:35 +0545

    MVC Assignment

---
+++ MVC/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult StudentList()
+        {
+            StudentList student = new StudentList("AAryan", "kathmandu");
+            return View(student);
+
+        }
+        
+        [HttpGet]
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DoLogin()
+        {
+
+            return View();
+        }
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        public IActionResult DoRegister()
+        {
+
+            return View();
+        }
+        public IActionResult ForgotPassword()
+        {
+
+            return View();
+        }
+        public IActionResult ChnagePassword()
+        {
+
+            return View();
+        }
+    }
+}
